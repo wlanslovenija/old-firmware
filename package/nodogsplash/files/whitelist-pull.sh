043b27fc443aaf4c7b9e887f371cb40a9257bc27
Jernej Kos <k@jst.sm>
2009-10-13 20:51:39 +0000

    Implemented MAC whitelist save and reload as per ticket #281.

--- package/nodogsplash/files/whitelist-pull.sh
+++ package/nodogsplash/files/whitelist-pull.sh
@@ -3,9 +3,22 @@
 trap 'killall ndsctl; exit' INT QUIT TERM EXIT
 
 NDSCTL="/usr/bin/ndsctl -s /var/run/nodogsplash.sock"
+WHITELIST_FILE="/etc/whitelist.mac"
 WHITELIST_URL="http://10.14.0.2/feeds/whitelist"
-WHITELIST="`wget -q -O - ${WHITELIST_URL} 2>/dev/null`"
-CURRENT_TRUSTS="`${NDSCTL} status | awk 'BEGIN { in_trust = 0 } !/^=/ && in_trust == 1 { print $1 } /^Trusted MAC addresses/ { in_trust = 1 } /^=/ { in_trust = 0 }'`"
+
+if [ "$1" == "init" ]; then
+  # Initial load, just use the file if one exists
+  if [ -f ${WHITELIST_FILE} ]; then
+    WHITELIST="`cat ${WHITELIST_FILE}`"
+    CURRENT_TRUSTS=""
+  fi
+else
+  WHITELIST="`wget -q -O - ${WHITELIST_URL} 2>/dev/null`"
+  CURRENT_TRUSTS="`${NDSCTL} status | awk 'BEGIN { in_trust = 0 } !/^=/ && in_trust == 1 { print $1 } /^Trusted MAC addresses/ { in_trust = 1 } /^=/ { in_trust = 0 }'`"
+
+  # Save whitelist to a file for quick reload on reboots
+  echo $WHITELIST > $WHITELIST_FILE
+fi
 
 # Remove some MACs from trusted list that were removed from the whitelist
 for trust in $CURRENT_TRUSTS; do
